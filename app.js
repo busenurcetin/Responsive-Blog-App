4cc84debe885761d0ee8bc24f3566e83c359df62
Buse Nur Çetin <busenurcet@gmail.com>
2023-04-14 16:02:22 +0300

    0.4 Buse Nur Çetin Last changes made added side bar, side bar toggle, manage categories,manage users,dashboard,category-posts, changes in app.js also in style.css. Project finished.

--- app.js
+++ app.js
@@ -20,3 +20,27 @@ const closeNav= () => {
 
 openNavBtn.addEventListener('click',openNav)
 closeNavBtn.addEventListener('click',closeNav)
+
+const sidebar= document.querySelector('aside');
+const showSidebarBtn = document.querySelector('#show__sidebar-btn');
+const hideSidebarBtn = document.querySelector('#hide__sidebar-btn');
+
+// Show Sidebar On Small Devices
+
+const showSidebar=()=> {
+    sidebar.style.left='0';
+    showSidebarBtn.style.display='none';
+    hideSidebarBtn.style.display='inline-block';
+}
+
+// Hide Sidebar On Small Devices
+
+const hideSidebar=()=> {
+    sidebar.style.left='-100%';
+    showSidebarBtn.style.display='inline-block';
+    hideSidebarBtn.style.display='none';
+}
+
+
+showSidebarBtn.addEventListener('click', showSidebar);
+hideSidebarBtn.addEventListener('click', hideSidebar);
